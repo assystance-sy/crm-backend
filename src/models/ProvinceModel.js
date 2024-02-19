868d127ff4758e2baf1c7a592f3c3beebccac998
Grace Cheng <gracesycheng@outlook.com>
2024-02-18 21:22:19 -0500

    [Update] Province - added country to schema

--- src/models/ProvinceModel.js
+++ src/models/ProvinceModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
+  country: { type: mongoose.ObjectId, ref: "Country" },
   name: { type: String },
   abbreviation: { type: String, uppercase: true, trim: true },
 });
