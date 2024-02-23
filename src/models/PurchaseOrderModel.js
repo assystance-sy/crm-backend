4a5d7becc9cd9f7e7776a0c495cea1c2b0065e36
Grace Cheng <gracesycheng@outlook.com>
2024-02-23 00:50:36 -0500

    [Update] Purchase order - updated schema

--- src/models/PurchaseOrderModel.js
+++ src/models/PurchaseOrderModel.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
 const { generateSerialNumber } = require("../../utils/functions");
 
-const schema = new mongoose.Schema({
-  merchant: { type: mongoose.ObjectId, ref: "Merchant" },
-  store: { type: mongoose.ObjectId, ref: "Store" },
-  orderNumber: { type: String, unique: true, immutable: true, required: true },
-});
-
-schema.virtual("items", {
-  ref: "PurchaseOrderItem",
-  localField: "_id",
-  foreignField: "purchaseOrder",
-});
+const schema = new mongoose.Schema(
+  {
+    merchant: { type: mongoose.ObjectId, ref: "Merchant" },
+    store: { type: mongoose.ObjectId, ref: "Store" },
+    orderNumber: {
+      type: String,
+      unique: true,
+      immutable: true,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
 schema.pre("validate", async function (next) {
   if (!this.orderNumber) {
